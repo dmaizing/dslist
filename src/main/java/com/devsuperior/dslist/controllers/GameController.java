5a7caa67f9c884c6a69c3c44b77193bb82de3324
dmaizing <davis.maizing@gmail.com>
2023-05-13 11:59:08 -0300

    Get By Id

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -4,14 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 
-@RestController
+@RestController 
 @RequestMapping(value = "/games")
 public class GameController {
 	
@@ -24,5 +25,10 @@ public class GameController {
 		return result;
 	}
 	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO res = gameService.findById(id);
+		return res;
+	}
 	
 }
